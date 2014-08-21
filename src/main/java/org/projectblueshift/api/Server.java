a95f07b0a1718be46924c96e82d03d3973262ad1
Harry Fox <harry.fox.ic@icloud.com>
2014-08-21 16:35:22 +0100

    Update the Server

--- src/main/java/org/projectblueshift/api/Server.java
+++ src/main/java/org/projectblueshift/api/Server.java
@@ -1,7 +1,13 @@
 package org.projectblueshift.api;
 
+import java.net.InetSocketAddress;
+
 public interface Server {
 
+	public InetSocketAddress getSocketAddress();
 
+	public short getPort();
+
+	public void shutdown();
 
 }
