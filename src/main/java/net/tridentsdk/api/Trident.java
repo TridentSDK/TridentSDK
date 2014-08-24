95fce71059c1abaf1d83d108b0f9299837368721
Mazen K <email@mazenmc.io>
2014-08-24 16:02:43 +0400

    Cleanup!

--- src/main/java/net/tridentsdk/api/Trident.java
+++ src/main/java/net/tridentsdk/api/Trident.java
@@ -3,17 +3,17 @@ package net.tridentsdk.api;
 import com.google.common.base.Preconditions;
 
 public final class Trident {
-	private static Server SERVER;
+	private static Server server;
 	
 	/**
 	 * Do not call.
 	 */
 	public static void setServer(Server s) {
-		Preconditions.checkArgument(SERVER == null, "Can only set server instance once!");
-		SERVER = s;
+		Preconditions.checkArgument(server != null, "Can only set server instance once!");
+		server = s;
 	}
 	
 	public static Server getServer() {
-		return SERVER;
+		return server;
 	}
 }
