e5634053b009c29a8f2748aabc89bbe5669e4b3f
Daniel Shell <theultimatebudgie@gmail.com>
2014-08-24 19:49:02 +1000

    Added access to the Server instance through the Trident class

--- src/main/java/net/tridentsdk/api/Trident.java
+++ src/main/java/net/tridentsdk/api/Trident.java
@@ -1,5 +1,19 @@
 package net.tridentsdk.api;
 
-public final class Trident {
+import com.google.common.base.Preconditions;
 
+public final class Trident {
+	private static Server SERVER;
+	
+	/**
+	 * Do not call.
+	 */
+	public static void setServer(Server s) {
+		Preconditions.checkArgument(SERVER == null, "Can only set server instance once!");
+		SERVER = s;
+	}
+	
+	public static Server getServer() {
+		return SERVER;
+	}
 }
