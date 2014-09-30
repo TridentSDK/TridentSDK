59e481bc78e1f9953a583d406cbc72fb28bb0d0d
Nickforall <nickvernij@outlook.com>
2014-09-30 21:15:14 +0200

    New functions in World
    Used the Level.dat file as a reference for function that might be useful
    for the API

--- src/main/java/net/tridentsdk/api/world/World.java
+++ src/main/java/net/tridentsdk/api/world/World.java
@@ -33,25 +33,167 @@ import net.tridentsdk.api.GameMode;
 import net.tridentsdk.api.Location;
 
 public interface World {
+    
+    /**
+     * Gets the name of the world
+     * 
+     * @return the name of the world
+     */
     String getName();
-
+    
+    /**
+     * Gets the chunk on the given location, and generates the chunk if it does not exist.
+     * 
+     * @return The chunk on the given location
+     */
     Chunk getChunkAt(ChunkLocation loc, boolean generateIfNotFound);
-
+    
+    /**
+     * Gets the chunk on the given x and z , and generates the chunk if it does not exist
+     * 
+     * @return The chunk on the given location
+     */
     Chunk getChunkAt(int x, int z, boolean generateIfNotFound);
-
+    
+    /**
+     * Generates the chunk on the given location
+     * 
+     * @return The generated chunk
+     */
     Chunk generateChunk(int x, int z);
-
+    
+    /**
+     * Generates the chunk on the given location
+     * 
+     * @return The generated chunk
+     */
     Chunk generateChunk(ChunkLocation location);
-
+    
+    /**
+     * Gets the block on the given location
+     * 
+     * @return The block on the given location
+     */
     Block getBlockAt(Location location);
-
+    
+    /**
+     * Gets the ChunkSnapshot
+     * 
+     * @return The ChunkSnapshot
+     */
     ChunkSnapshot getChunkSnapshot();
-
-    Dimension getDimesion();
-
+    
+    /**
+     * Gets the dimension of a world
+     * 
+     * @return The dimension of a world
+     */
+    Dimension getDimension();
+    
+    /**
+     * Gets the difficulty set in a world
+     * 
+     * @return The difficulty set in a world
+     */
     Difficulty getDifficulity();
-
+    
+    /**
+     * Gets the default gamemode in a given chunk
+     * 
+     * @return The default gamemode in a given chunk
+     */
     GameMode getDefaultGamemode();
-
+    
+    /**
+     * Gets the type of a world
+     * 
+     * @return The type of a world
+     */
     LevelType getLevelType();
+    
+    /**
+     * Gets the set boolean for the given gamerule
+     * 
+     * @return The set boolean for the given gamerule
+     */
+    boolean getGamerule(String rule);
+    
+    /**
+     * Gets the time in a world
+     * 
+     * @return The time in a world
+     */
+    long getTime();
+    
+    /**
+     * Gets the spawn location of a world
+     * 
+     * @return The spawn location in a world
+     */
+    Location getSpawn();
+    
+    /**
+     * Checks if it is raining in a world
+     * 
+     * @return True if it is raining in a world
+     */
+    boolean isRaining();
+    
+    /**
+     * Gets the number of ticks before raining is toggled
+     * 
+     * @return The number of ticks before raining is toggled
+     */
+    int getRainTime();
+    
+    /**
+     * Checks if it is thundering in a world
+     * 
+     * @return True if it is thundering in a world
+     */
+    boolean isThundering();
+    
+    /**
+     * Gets the number of ticks before thundering is toggled
+     * 
+     * @return The number of ticks before thundering is toggled
+     */
+    int getThunderTime();
+    
+    /**
+     * Checks if structures are generated in a world (Stronghold, villages, dungeons)
+     * 
+     * @return True if structures are generated in a world (Stronghold, villages, dungeons)
+     */
+    boolean canGenerateStructures();
+    
+    /**
+     * Gets the size of the worldborder
+     * 
+     * @return The size of the worldborder
+     */
+    int getBorderSize();
+    
+    /**
+     * Gets the location where the worldborder is centered
+     * 
+     * @return The location where the worldborder is centered
+     */
+    Location getBorderCenter();
+    
+    /**
+     * Gets to what size a border is contracting, 60000000 by default
+     * 
+     * @return To what size a border is contracting, 60000000 by default
+     */
+    int getBorderSizeContraction();
+    
+    /**
+     * Gets the time the border has to contract to the contraction target
+     * 
+     * @return The time the border has to contract to the contraction target
+     */
+    int getBorderSizeContractionTime();
+    
+    
 }
