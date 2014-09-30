31b070800923d6eb82dd26dc22878919fcf64f2d
mythbusterma <wiirocksme@gmail.com>
2014-09-30 19:06:21 -0400

    Unbreak Git

--- src/main/java/net/tridentsdk/api/event/player/PlayerGameModeChangeEvent.java
+++ src/main/java/net/tridentsdk/api/event/player/PlayerGameModeChangeEvent.java
@@ -1,6 +1,6 @@
 package net.tridentsdk.api.event.player;
 
-import net.tridentsdk.api.Gamemode;
+import net.tridentsdk.api.GameMode;
 import net.tridentsdk.api.entity.living.Player;
 import net.tridentsdk.api.event.Cancellable;
 
@@ -8,11 +8,11 @@ import net.tridentsdk.api.event.Cancellable;
  * Called <i>before</i> a Player's game mode changes
  */
 public class PlayerGameModeChangeEvent extends PlayerEvent implements Cancellable {
-    private Gamemode gameMode;
+    private GameMode gameMode;
 
     private boolean cancelled;
 
-    public PlayerGameModeChangeEvent(Player player, Gamemode gameMode) {
+    public PlayerGameModeChangeEvent(Player player, GameMode gameMode) {
         super(player);
         this.gameMode = gameMode;
     }
@@ -25,15 +25,15 @@ public class PlayerGameModeChangeEvent extends PlayerEvent implements Cancellabl
         this.cancelled = cancelled;
     }
 
-    public Gamemode getNewGameMode() {
+    public GameMode getNewGameMode() {
         return gameMode;
     }
 
-    public Gamemode getCurrentGameMode() {
+    public GameMode getCurrentGameMode() {
         return getPlayer().getGameMode();
     }
 
-    public void setNewGamemode(Gamemode gameMode) {
+    public void setNewGamemode(GameMode gameMode) {
         this.gameMode = gameMode;
     }
 }
