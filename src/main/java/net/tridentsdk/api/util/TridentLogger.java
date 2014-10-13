699efb8d6c6e8322c1ffba180bb0975e2593daca
AgentTroll <woodyc40@gmail.com>
2014-10-13 15:06:04 -0700

    Begin logging

--- src/main/java/net/tridentsdk/api/util/TridentLogger.java
+++ src/main/java/net/tridentsdk/api/util/TridentLogger.java
@@ -30,10 +30,7 @@ package net.tridentsdk.api.util;
 import java.util.logging.Logger;
 
 public class TridentLogger extends Logger {
-
     public TridentLogger() {
         super("Trident", null);
     }
-
-    // TODO: override methods to fit Trident
 }
