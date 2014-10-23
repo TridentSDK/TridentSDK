1cc5f81137c8caeb878cea6a3209717781417925
Mazen K <email@mazenmc.io>
2014-10-23 13:54:11 -0700

    Made ConfigList use a LinkedList implementation

--- src/main/java/net/tridentsdk/api/config/ConfigList.java
+++ src/main/java/net/tridentsdk/api/config/ConfigList.java
@@ -19,105 +19,136 @@ package net.tridentsdk.api.config;
 
 import com.google.gson.JsonArray;
 
-import java.util.ArrayList;
+import java.util.AbstractList;
 import java.util.Collection;
 import java.util.List;
 
 // TODO: Javadoc
+
 /**
- * An ArrayList that also makes changes to the underlying JsonArray object
+ * An LinkedList [implementation] that also makes changes to the underlying JsonArray object
  *
  * @author The TridentSDK Team
  */
-public class ConfigList<V> extends ArrayList<V> {
+public final class ConfigList<V> extends AbstractList<V> implements List<V>, Iterable<V> {
     private static final long serialVersionUID = -7535821700183585211L;
 
     JsonArray jsonHandle;
 
+    private Node<V> head;
+
+    private int size = 0;
+
     protected ConfigList(JsonArray handle) {
         this.jsonHandle = handle;
+        head = new Node<>(null, null);
+    }
+
+    protected ConfigList(JsonArray handle, Collection<V> c) {
+        this(handle);
+        addAll(c);
+    }
+
+    @Override
+    public V get(int index) {
+        checkElementIndex(index);
+        return getNode(index).value;
+    }
+
+    @Override
+    public int size() {
+        return size;
     }
 
     @Override
     public boolean add(V element) {
-        boolean changed = super.add(element);
-        if (changed) {
-            this.jsonHandle.add(GsonFactory.getGson().toJsonTree(element));
-        }
-        return changed;
+        Node<V> last = getNode(size - 1);
+
+        last.next = new Node<>(element, null);
+        size += 1;
+        modCount += 1;
+
+        this.jsonHandle.add(GsonFactory.getGson().toJsonTree(element));
+        return true;
     }
 
     @Override
     public boolean addAll(Collection<? extends V> coll) {
-        boolean changed = super.addAll(coll);
-        if (changed) {
-            for (V element : coll) {
-                this.add(element);
-            }
+        for (V element : coll) {
+            this.add(element);
         }
-        return changed;
+
+        return true;
     }
 
     /* (non-Javadoc)
-     * @see java.util.ArrayList#set(int, java.lang.Object)
+     * @see java.util.List#set(int, java.lang.Object)
      */
     @Override
     public V set(int index, V element) {
+        checkElementIndex(index);
         this.jsonHandle.set(index, GsonFactory.getGson().toJsonTree(element));
-        return super.set(index, element);
+
+        Node<V> node = getNode(index);
+        final V oldValue = node.value;
+
+        node.value = element;
+
+        return oldValue;
     }
 
     @Override
     public V remove(int index) {
+        checkElementIndex(index);
         this.jsonHandle.remove(index);
-        return super.remove(index);
+
+        Node<V> previous = getNode(index - 1);
+        final V value = previous.next.value;
+        previous.next = previous.next.next;
+
+        return value;
     }
 
     @Override
     public boolean remove(Object element) {
-        boolean success = super.remove(element);
-        if (success) {
-            this.jsonHandle.remove(GsonFactory.getGson().toJsonTree(element));
-        }
-        return success;
+        remove(indexOf(element));
+        return true;
     }
 
     /* (non-Javadoc)
-     * @see java.util.ArrayList#removeAll(java.util.Collection)
+     * @see java.util.List#removeAll(java.util.Collection)
      */
     @Override
     public boolean removeAll(Collection<?> coll) {
-        boolean changed = super.removeAll(coll);
-        if (changed) {
-            for (Object o : coll) {
-                this.remove(o);
-            }
+        for (Object o : coll) {
+            this.remove(o);
         }
-        return changed;
+
+        return true;
     }
 
     /* (non-Javadoc)
-     * @see java.util.ArrayList#removeRange(int, int)
+     * @see java.util.List#removeRange(int, int)
      */
     @Override
     protected void removeRange(int start, int end) {
-        super.removeRange(start, end);
         for (int i = start; i < end; i++) {
             this.jsonHandle.remove(i);
+            remove(i);
         }
     }
 
     /* (non-Javadoc)
-     * @see java.util.ArrayList#clear()
+     * @see java.util.List#clear()
      */
     @Override
     public void clear() {
-        super.clear();
+        removeAll(new ConfigList<>(jsonHandle, this));
         this.jsonHandle = new JsonArray();
     }
 
     /* (non-Javadoc)
-     * @see java.util.ArrayList#add(int, java.lang.Object)
+     * @see java.util.List#add(int, java.lang.Object)
      */
     @Override
     public void add(int index, V element) {
@@ -125,7 +156,7 @@ public class ConfigList<V> extends ArrayList<V> {
     }
 
     /* (non-Javadoc)
-     * @see java.util.ArrayList#addAll(int, java.util.Collection)
+     * @see java.util.List#addAll(int, java.util.Collection)
      */
     @Override
     public boolean addAll(int arg0, Collection<? extends V> arg1) {
@@ -133,7 +164,7 @@ public class ConfigList<V> extends ArrayList<V> {
     }
 
     /* (non-Javadoc)
-     * @see java.util.ArrayList#retainAll(java.util.Collection)
+     * @see java.util.List#retainAll(java.util.Collection)
      */
     @Override
     public boolean retainAll(Collection<?> arg0) {
@@ -141,11 +172,7 @@ public class ConfigList<V> extends ArrayList<V> {
     }
 
     /* (non-Javadoc)
-     * @see java.util.ArrayList#spliterator()
-     */
-
-    /* (non-Javadoc)
-     * @see java.util.ArrayList#subList(int, int)
+     * @see java.util.List#subList(int, int)
      */
     @Override
     public List<V> subList(int arg0, int arg1) {
@@ -153,18 +180,43 @@ public class ConfigList<V> extends ArrayList<V> {
     }
 
     /* (non-Javadoc)
-     * @see java.util.ArrayList#toArray()
+     * @see java.util.List#toArray()
      */
     @Override
-    public Object[] toArray() {
+    public V[] toArray() {
         throw new UnsupportedOperationException("Cannot invoke on Lists from Config");
     }
 
     /* (non-Javadoc)
-     * @see java.util.ArrayList#toArray(java.lang.Object[])
+     * @see java.util.List#toArray(java.lang.Object[])
      */
     @Override
     public <T> T[] toArray(T[] arg0) {
         throw new UnsupportedOperationException("Cannot invoke on Lists from Config");
+    }
+
+    private void checkElementIndex(int index) {
+        if (index < 0 && index > size)
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+    }
+
+    private Node<V> getNode(int index) {
+        Node<V> node = head;
+
+        for(int i = 0; i < index && node.next != null; i += 1) {
+            node = node.next;
+        }
+
+        return node;
+    }
+
+    private static class Node<V> {
+        V value;
+        Node<V> next;
+
+        private Node(V value, Node<V> next) {
+            this.value = value;
+            this.next = next;
+        }
     }
 }
