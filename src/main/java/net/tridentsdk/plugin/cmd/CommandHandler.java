3add0dfacd160eae061e694fff24a43c18f9d28d
Max Dubrinsky <mimbsky102@gmail.com>
2014-11-20 16:54:26 -0500

    Changed Command Package to cmd

--- src/main/java/net/tridentsdk/plugin/cmd/CommandHandler.java
+++ src/main/java/net/tridentsdk/plugin/cmd/CommandHandler.java
@@ -15,7 +15,7 @@
  *     You should have received a copy of the GNU General Public License
  *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package net.tridentsdk.plugin.Command;
+package net.tridentsdk.plugin.cmd;
 
 
 import net.tridentsdk.api.CommandIssuer;
