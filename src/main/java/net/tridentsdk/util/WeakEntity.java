daa2f040340b901db57daa4192d1fae7fc0902ea
AgentTroll <woodyc40@gmail.com>
2015-01-20 21:28:45 -0800

    Finished WeakEntity

--- src/main/java/net/tridentsdk/util/WeakEntity.java
+++ src/main/java/net/tridentsdk/util/WeakEntity.java
@@ -1,24 +1,35 @@
+/*
+ * Trident - A Multithreaded Server Alternative
+ * Copyright 2014 The TridentSDK Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package net.tridentsdk.util;
 
-import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
-import net.tridentsdk.Coordinates;
 import net.tridentsdk.Trident;
 import net.tridentsdk.docs.InternalUseOnly;
 import net.tridentsdk.entity.Entity;
-import net.tridentsdk.entity.EntityProperties;
-import net.tridentsdk.entity.EntityType;
-import net.tridentsdk.factory.CollectFactory;
-import net.tridentsdk.factory.Factories;
-import net.tridentsdk.world.World;
 
 import javax.annotation.concurrent.GuardedBy;
 import javax.annotation.concurrent.ThreadSafe;
 import java.lang.ref.WeakReference;
-import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
@@ -45,7 +56,8 @@ import java.util.concurrent.locks.ReentrantLock;
  * in which case is indicated by {@link #isNull()} returning {@code true}.</p>
  *
  * <p>Much thought was put into the design of this class, especially if it should be made thread-safe. Because this is
- * an internal change, it can be easily done without breaking the API. However, the decision was made to make this class
+ * an internal change, it can be easily done without breaking the API. However, the decision was made to make this
+ * class
  * thread-safe in order to preserve the fact that both the users of this API, including plugins and the server itself
  * is concurrent. It would decrease the burden to handle this safely using the internal implementation rather than
  * having the client code execute awkward constructs in order to keep consistency. This is OK for the implementation to
@@ -54,16 +66,23 @@ import java.util.concurrent.locks.ReentrantLock;
  *
  * <p>This is the equivalent of {@link com.google.common.base.Optional} but for entities specifically, much like an
  * optional which stores a {@link java.lang.ref.WeakReference} of the specified property. While this class does use
- * WeakReference to manage the references, other methods to safeguard the reference is also implemented, such as removal
+ * WeakReference to manage the references, other methods to safeguard the reference is also implemented, such as
+ * removal
  * listeners.</p>
  *
  * <p>Internally, this class manages a queue of references which, in turn, are polled by a thread called
  * {@code Trident - Reference Handler}. Every call to clear a specific reference also runs the mark/sweep collector,
- * although it is invoked directly rather than using {@link #runMarkSweep()}. The implementation of the collector may be
+ * although it is invoked directly rather than using {@link #runMarkSweep()}. The implementation of the collector may
+ * be
  * found in the documentation for {@link  #runMarkSweep()}. Entities call
- * {@link #clearReferencesTo(net.tridentsdk.entity.Entity)} when they are removed to purge old references to that entity
+ * {@link #clearReferencesTo(net.tridentsdk.entity.Entity)} when they are removed to purge old references to that
+ * entity
  * after it has finished despawning. This method also runs the mark/sweep collector. This is only implemented as a
- * safeguard behind {@link java.lang.ref.WeakReference}, which is used to store the </p>
+ * safeguard behind {@link java.lang.ref.WeakReference}, which is used to store the actual instance of the entity to be
+ * weakly referenced. The reference handling thread cleans up the entity after all the references have been cleared, as
+ * well as collecting any other obsolete references. This was done because WeakReference may allow the reference to be
+ * held longer than it has lived, in the case that the GC does not run, the reference handler will perform the
+ * necessary actions to make sure the entity is correctly nulled.</p>
  *
  * <p>Here are examples on the usage of this class.</p>
  *
@@ -176,7 +195,7 @@ public final class WeakEntity<T extends Entity> {
      * {@link #orEmpty(net.tridentsdk.entity.Entity)}.</p>
      *
      * @param referencedEntity the entity to hold until it is removed
-     * @param <T> the type of entity to hold
+     * @param <T>              the type of entity to hold
      * @return a WeakEntity which holds the entity until it becomes {@code null}
      */
     public static <T extends Entity> WeakEntity<T> of(T referencedEntity) {
@@ -195,9 +214,9 @@ public final class WeakEntity<T extends Entity> {
      * still correct, if it is not possible for it to be {@code null}, use an {@link #empty()} WeakEntity.</p>
      *
      * @param referencedEntity the entity to obtain the reference for
-     * @param <T> the type of entity to hold
+     * @param <T>              the type of entity to hold
      * @return a WeakEntity which holds the specified entity, or {@link #empty()} if the specified entity to reference
-     *         is {@code null}
+     * is {@code null}
      */
     public static <T extends Entity> WeakEntity<T> orEmpty(T referencedEntity) {
         if (referencedEntity == null)
@@ -224,8 +243,10 @@ public final class WeakEntity<T extends Entity> {
      * Forces the reference handler thread to run the {@code null} or garbage reference collection cycle and reclaim
      * memory lost by those references
      *
-     * <p>This runs the mark sweeping strategy employed by the reference handler. Previous references are swept from the
-     * session and the thread continues to mark the {@code null} references. They are placed into a collection until the
+     * <p>This runs the mark sweeping strategy employed by the reference handler. Previous references are swept from
+     * the
+     * session and the thread continues to mark the {@code null} references. They are placed into a collection until
+     * the
      * sweeping completes. When this step does complete, the collection is checked, then the references are purged from
      * the reference queue.</p>
      *
@@ -240,10 +261,10 @@ public final class WeakEntity<T extends Entity> {
      * Observes the reference to see if the entity is {@code null} or not accessible via the server anymore
      *
      * @return {@code true} to indicate the entity is removed and cannot be further referenced, in which case the
-     *         instance of this object should also become {@code null}
+     * instance of this object should also become {@code null}
      */
     public boolean isNull() {
-        return referencedEntity.get() == null;
+        return entity() == null;
     }
 
     /**
@@ -301,22 +322,20 @@ public final class WeakEntity<T extends Entity> {
         if (!(obj instanceof WeakEntity))
             return false;
         WeakEntity entity = (WeakEntity) obj;
-        return entity.entity().equals(this.entity());
+        return (isNull() && entity.isNull()) || this.entity().equals(entity.entity());
     }
 
     @Override
     public int hashCode() {
-        T ref = referencedEntity.get();
-        if (ref == null)
-            return 0;
-        return ref.hashCode();
+        return Objects.hashCode(referencedEntity);
     }
 
     @Override
     public String toString() {
-        return getClass() + "{referencedEntity = " + referencedEntity.get() + "}@" + hashCode();
+        return getClass().getName() + "{referencedEntity = " + referencedEntity.get() + "}@" + hashCode();
     }
 
+    // Thread-visible version of WeakReference
     private static class SafeReference<T> extends WeakReference<T> {
         private volatile int fence = 0;
 
@@ -337,163 +356,23 @@ public final class WeakEntity<T extends Entity> {
         }
     }
 
+    // Stores references and collects/cleans up null references
     private static class RefQueue implements Runnable {
-        static {
-            Factories.init(new CollectFactory() {
-                @Override
-                public <K, V> ConcurrentMap<K, V> createMap() {
-                    return new ConcurrentHashMap<>();
-                }
-            });
-        }
-
         @GuardedBy("lock")
-        private final Map<Entity, Node> references = Maps.newTreeMap(new Comparator<Object>() {
-            @Override
-            public int compare(Object o1, Object o2) {
-                return 0;
-            }
-        });
+        private final Map<Entity, Node> references = Maps.newHashMap();
 
+        // Locking mechanisms
         private final Lock lock = new ReentrantLock();
         private final Condition hasNodes = lock.newCondition();
 
-        private volatile Node marked = Node.newNode(null, null);
-        private final List<Entity> old = Lists.newArrayList();
+        // INVARIANT: MODIFIED THREAD-LOCALLY ONLY
+        private final Set<WeakEntity> marked = Sets.newHashSet();
 
         private RefQueue() {
         }
 
         public static RefQueue newQueue() {
             return new RefQueue();
-        }
-
-        public static void main(String[] args) {
-            RefQueue queue = RefQueue.newQueue();
-            Entity entity = new Entity() {
-                @Override
-                public void teleport(double x, double y, double z) {
-
-                }
-
-                @Override
-                public void teleport(Entity entity) {
-
-                }
-
-                @Override
-                public void teleport(Coordinates location) {
-
-                }
-
-                @Override
-                public World world() {
-                    return null;
-                }
-
-                @Override
-                public Coordinates location() {
-                    return null;
-                }
-
-                @Override
-                public Vector velocity() {
-                    return null;
-                }
-
-                @Override
-                public void setVelocity(Vector vector) {
-
-                }
-
-                @Override
-                public boolean onGround() {
-                    return false;
-                }
-
-                @Override
-                public Set<Entity> withinRange(double radius) {
-                    return null;
-                }
-
-                @Override
-                public String displayName() {
-                    return null;
-                }
-
-                @Override
-                public void setDisplayName(String name) {
-
-                }
-
-                @Override
-                public boolean isNameVisible() {
-                    return false;
-                }
-
-                @Override
-                public boolean isSilent() {
-                    return false;
-                }
-
-                @Override
-                public int entityId() {
-                    return 0;
-                }
-
-                @Override
-                public UUID uniqueId() {
-                    return null;
-                }
-
-                @Override
-                public void remove() {
-
-                }
-
-                @Override
-                public Entity passenger() {
-                    return null;
-                }
-
-                @Override
-                public void setPassenger(Entity entity) {
-
-                }
-
-                @Override
-                public void eject() {
-
-                }
-
-                @Override
-                public EntityType type() {
-                    return null;
-                }
-
-                @Override
-                public void applyProperties(EntityProperties properties) {
-
-                }
-            };
-
-            new Thread(queue).start();
-
-            WeakEntity ref = queue.put(entity);
-
-            System.out.println(queue.references.get(entity).references().contains(ref));
-            queue.beginSweep();
-            System.out.println(queue.references.get(entity).references().contains(ref));
-            ref.clear();
-            System.out.println(queue.references.containsKey(entity));
-            queue.beginSweep();
-            System.out.println(queue.references.containsKey(entity));
-            queue.clearReference(entity);
-            System.out.println(queue.references.containsKey(entity));
-        }
-
-        public static void find(RefQueue queue, Entity entity) {
-            System.out.println(queue.references.get(entity).references().iterator().next());
         }
 
         public WeakEntity put(Entity entity) {
@@ -509,6 +388,8 @@ public final class WeakEntity<T extends Entity> {
             return weakEntity;
         }
 
+        // Clears references to the entity, forcing the sweep to run
+        // this leaves the empty node to be collected by the sweeper
         public void clearReference(Entity entity) {
             Node entities = null;
 
@@ -520,7 +401,8 @@ public final class WeakEntity<T extends Entity> {
                 lock.unlock();
             }
 
-            if (entities == null) return;
+            if (entities == null)
+                return;
             for (WeakEntity<?> e : entities)
                 e.clear();
         }
@@ -549,39 +431,40 @@ public final class WeakEntity<T extends Entity> {
 
                 // Step 2: mark
                 for (Map.Entry<Entity, Node> entry : entries) {
-                    System.out.println(entry.getKey() + " " + entry.getValue());
                     Set<WeakEntity> weakRefs = entry.getValue().references();
 
                     for (WeakEntity ref : weakRefs) {
-                        System.out.println(ref.toString());
                         if (ref.isNull()) {
-                            marked = Node.newNode(ref, marked);
+                            marked.add(ref);
                         }
-                    }
-
-                    // Remove old keys
-                    if (weakRefs.isEmpty()) {
-                        old.add(entry.getKey());
-                        return;
                     }
                 }
 
-
                 try {
+                    // This entire method doesn't use finally blocks
+                    // We clean up later just in case
+
                     lock.lockInterruptibly();
-                    while (references.isEmpty())
+                    while (marked.isEmpty())
                         hasNodes.await();
+                    lock.unlock();
 
                     // Step 3: sweep
-                    for (Entity entity : references.keySet())
-                        references.get(entity).references().removeAll(marked.references());
+                    for (Map.Entry<Entity, Node> entry : entries) {
+                        Set<WeakEntity> refs = entry.getValue().references();
+                        refs.removeAll(marked);
 
-                    marked.clean();
+                        if (refs.isEmpty()) {
+                            lock.lock();
+                            // This happens very rarely, it is OK to waste time
+                            // to acquire the lock again
+                            references.remove(entry.getKey());
+                            lock.unlock();
+                        }
+                    }
 
-                    for (Entity entity : old)
-                        references.remove(entity);
-
-                    old.clear();
+                    // Remove marked entries
+                    marked.clear();
                 } catch (InterruptedException ignored) {
                     // Run by a daemon thread, doesn't matter if it was interrupted
                 } finally {
@@ -591,6 +474,7 @@ public final class WeakEntity<T extends Entity> {
         }
     }
 
+    // A set of references assigned to a particular entity
     private static class Node implements Iterable<WeakEntity> {
         @GuardedBy("lock")
         private final Set<WeakEntity> weakEntities;
@@ -600,7 +484,8 @@ public final class WeakEntity<T extends Entity> {
             Set<WeakEntity> original;
             if (node == null)
                 original = Sets.newHashSet();
-            else original = node.weakEntities;
+            else
+                original = node.weakEntities;
 
             if (original == null)
                 original = Sets.newHashSet();
@@ -613,15 +498,17 @@ public final class WeakEntity<T extends Entity> {
             return new Node(entity, node);
         }
 
+        // Removes the references which are empty
         public void clean() throws InterruptedException {
             synchronized (lock) {
                 for (WeakEntity entity : weakEntities) {
-                    if (entity == null || entity.isNull())
+                    if (entity.isNull())
                         weakEntities.remove(entity);
                 }
             }
         }
 
+        // Removes a specified entity
         public void clear(WeakEntity entity) {
             synchronized (lock) {
                 weakEntities.remove(entity);
