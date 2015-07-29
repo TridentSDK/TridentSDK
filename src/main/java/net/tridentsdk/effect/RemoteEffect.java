0cdf64d9d8e992c7d33ab205abad1b3d2213ab52
Vilsol <vilsol2000@gmail.com>
2015-07-29 07:38:17 +0300

    Effects only need vectors without a world

--- src/main/java/net/tridentsdk/effect/RemoteEffect.java
+++ src/main/java/net/tridentsdk/effect/RemoteEffect.java
@@ -1,7 +1,7 @@
 package net.tridentsdk.effect;
 
-import net.tridentsdk.base.Position;
 import net.tridentsdk.entity.living.Player;
+import net.tridentsdk.util.Vector;
 
 /**
  * Represents effects that can be executed without an entity
@@ -14,23 +14,23 @@ public interface RemoteEffect<T> extends Effect<T> {
     /**
      * Execute the effect at the given location for all nearby players
      *
-     * @param position The position of the effect
+     * @param vector The vector of the effect
      */
-    void apply(Position position);
+    void apply(Vector vector);
 
     /**
      * Execute the effect at the given location for specified player
      *
      * @param player The player to send the effect to
-     * @param position The position of the effect
+     * @param vector The vector of the effect
      */
-    void apply(Player player, Position position);
+    void apply(Player player, Vector vector);
 
     /**
      * Set the position of the effect
      *
-     * @param position The position of the effect
+     * @param vector The vector of the effect
      */
-    void setPosition(Position position);
+    void setPosition(Vector vector);
 
 }
