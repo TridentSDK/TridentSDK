8f543033c3f5323ad8ef2034f2ec4768840c9b03
Vilsol <vilsol2000@gmail.com>
2015-08-03 08:32:21 +0300

    Bash is so picky... not even double brackets...

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 BRANCH=$(git status | grep "On branch" | awk '{print $3}')
 
-if [[ "$BRANCH" == "bleeding-edge" ]];
+if [ "$BRANCH" == "bleeding-edge" ];
 then
         mvn clean install deploy --settings target/travis/settings.xml
 else
