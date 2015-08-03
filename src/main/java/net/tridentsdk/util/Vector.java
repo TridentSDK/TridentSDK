3bbf7c918502f1338fd2ad66bc81b1325cb6099f
AgentTroll <woodyc40@gmail.com>
2015-08-03 13:46:45 -0700

    Work on TODOs

--- src/main/java/net/tridentsdk/util/Vector.java
+++ src/main/java/net/tridentsdk/util/Vector.java
@@ -109,7 +109,7 @@ public class Vector implements Serializable, Cloneable {
      */
     public Vector add(int x, int y, int z) {
         return this.add((double) x, (double) y, (double) z);
-        /* TODO: Implementation detail:
+        /* Implementation detail:
         DO NOT CREATE A NEW VECTOR HERE JUST BECAUSE (!)
         Doing so wastes memory and adds unnecessary object
         creation overhead, therefore, delegate to the
