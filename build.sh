42abff1ce6e593cebce1d5b326a90b29050153f2
Vilsol <Vilsol@users.noreply.github.com>
2015-08-09 21:35:31 +0300

    Don't deploy pull requests

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 echo "Currently on branch: $TRAVIS_BRANCH"
 
-if [ "$TRAVIS_BRANCH" == "bleeding-edge" ];
+if [ "$TRAVIS_BRANCH" == "bleeding-edge" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ];
 then
     echo "Compiling with deployment"
     mvn clean install deploy --settings target/travis/settings.xml
