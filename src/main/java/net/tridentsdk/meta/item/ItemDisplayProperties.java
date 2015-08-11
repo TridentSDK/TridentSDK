2280a133b6ab84b75e98479cc17d6c9476133e9b
nickrobson <robson.nicholas1@gmail.com>
2015-08-11 13:30:16 +1000

    Fix potential NPE

--- src/main/java/net/tridentsdk/meta/item/ItemDisplayProperties.java
+++ src/main/java/net/tridentsdk/meta/item/ItemDisplayProperties.java
@@ -19,6 +19,8 @@ package net.tridentsdk.meta.item;
 import java.util.Arrays;
 import java.util.List;
 
+import com.google.common.collect.Lists;
+
 import net.tridentsdk.meta.nbt.NBTSerializable;
 
 /**
@@ -66,6 +68,8 @@ public interface ItemDisplayProperties extends NBTSerializable {
      */
     default void addLore(int index, String... lore) {
         List<String> curr = lore();
+        if (curr == null)
+            curr = Lists.newArrayList();
         if (curr.isEmpty() || index == curr.size()) {
             curr.addAll(Arrays.asList(lore));
         } else {
