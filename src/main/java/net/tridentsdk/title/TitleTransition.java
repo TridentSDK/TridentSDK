17666566f24d89868e0bfb33095a616b6e7aa2aa
PizzaCrust <torchprojectserver@gmail.com>
2015-12-24 10:29:20 +0700

    Added whitespacing to the javadocs, and added copyright.

--- src/main/java/net/tridentsdk/title/TitleTransition.java
+++ src/main/java/net/tridentsdk/title/TitleTransition.java
@@ -1,3 +1,19 @@
+/*
+ * Trident - A Multithreaded Server Alternative
+ * Copyright 2014 The TridentSDK Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.tridentsdk.title;
 
 /**
@@ -9,10 +25,12 @@ public class TitleTransition {
      * Time of fade in
      */
     private int fadeIn;
+
     /**
      * Time of the title showing
      */
     private int time;
+
     /**
      * Time of fade out
      */
@@ -20,8 +38,11 @@ public class TitleTransition {
 
     /**
      * Creates a title transition object
+     *
      * @param fadeIn the fade in time
+     *
      * @param time the time of the title
+     *
      * @param fadeOut the fade out time
      */
     public TitleTransition(int fadeIn, int time, int fadeOut){
@@ -32,6 +53,7 @@ public class TitleTransition {
 
     /**
      * Gets the fade in time of the transition
+     *
      * @return the fade in time
      */
     public int getFadeInTime(){
@@ -40,6 +62,7 @@ public class TitleTransition {
 
     /**
      * Gets the title time of the transition
+     *
      * @return the title time
      */
     public int getTitleTime(){
@@ -48,6 +71,7 @@ public class TitleTransition {
 
     /**
      * Gets the fade out time of the transition
+     *
      * @return the fade out time
      */
     public int getFadeOutTime(){
