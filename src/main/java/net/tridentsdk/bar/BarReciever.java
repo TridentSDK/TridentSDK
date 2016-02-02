be9e3e1832d4b74c73c347dc8da790bc459ce2bb
PizzaCrust <torchprojectserver@gmail.com>
2016-02-02 17:38:47 +0700

    BarReciever, BarType, and a extension in Player.

---
+++ src/main/java/net/tridentsdk/bar/BarReciever.java
@@ -0,0 +1,24 @@
+package net.tridentsdk.bar;
+
+/**
+ * A player entity or something that can recieve bars.
+ *
+ * @author The TridentSDK Team
+ */
+public interface BarReciever {
+    /**
+     * Sends a bar accordingly to the type and the message.
+     *
+     * @param barType the type of bar
+     * @param message the message on the bar
+     */
+    void sendBar(BarType barType, String message);
+
+    /**
+     * Sends a bar accordingly to the type id and the message.
+     *
+     * @param typeId the type id of the bar
+     * @param message the message on the bar
+     */
+    void sendBar(int typeId, String message);
+}
---
+++ src/main/java/net/tridentsdk/bar/BarType.java
@@ -0,0 +1,26 @@
+package net.tridentsdk.bar;
+
+/**
+ * Enum of all bar types.
+ *
+ * @author The TridentSDK Team
+ */
+public enum BarType {
+    ACTION_BAR(0),
+    BOSS_BAR(1);
+
+    private int id;
+
+    BarType(int id) {
+        this.id = id;
+    }
+
+    /**
+     * Get the id value of the enum
+     *
+     * @return the id value
+     */
+    public int id() {
+        return id;
+    }
+}
