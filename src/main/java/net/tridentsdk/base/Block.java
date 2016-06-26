75f7c52c3d1d02a03b451f74d7ec01051f78edfb
AgentTroll <woodyc40@gmail.com>
2016-06-26 15:12:35 +0700

    Remove unneeded test files

--- src/main/java/net/tridentsdk/base/Block.java
+++ src/main/java/net/tridentsdk/base/Block.java
@@ -14,7 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package net.tridentsdk.base;
 
 import javax.annotation.concurrent.ThreadSafe;
