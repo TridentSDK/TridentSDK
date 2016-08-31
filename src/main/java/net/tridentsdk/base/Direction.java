289bad8f0309c09dcccc90061a1bf1d2d1c3444f
Nick Robson <robson.nicholas1@gmail.com>
2016-08-31 22:29:02 +1000

    Documentation for Direction.

--- src/main/java/net/tridentsdk/base/Direction.java
+++ src/main/java/net/tridentsdk/base/Direction.java
@@ -1,16 +1,41 @@
 package net.tridentsdk.base;
 
 /**
+ * Represents a direction
+ *
  * @author TridentSDK
  * @since 0.5-alpha
  */
 public enum Direction {
 
+    /**
+     * Downwards
+     */
     DOWN,
+
+    /**
+     * Upwards
+     */
     UP,
+
+    /**
+     * North
+     */
     NORTH,
+
+    /**
+     * South
+     */
     SOUTH,
+
+    /**
+     * West
+     */
     WEST,
+
+    /**
+     * East
+     */
     EAST
 
 }
