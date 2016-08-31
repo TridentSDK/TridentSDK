1d9fa1e01841038e577feb29b9e8b625be50625a
Nick Robson <robson.nicholas1@gmail.com>
2016-08-31 22:31:26 +1000

    clarify ordering of Direction members

--- src/main/java/net/tridentsdk/base/Direction.java
+++ src/main/java/net/tridentsdk/base/Direction.java
@@ -1,7 +1,10 @@
 package net.tridentsdk.base;
 
 /**
- * Represents a direction
+ * Represents a direction.
+ *
+ * <p>Please note, the order of the elements in this class IS IMPORTANT.</p>
+ *
  *
  * @author TridentSDK
  * @since 0.5-alpha
