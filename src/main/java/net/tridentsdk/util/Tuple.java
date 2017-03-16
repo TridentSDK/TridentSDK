42b069476e534b8609296608edf297e8ee415620
AgentTroll <woodyc40@gmail.com>
2017-03-15 22:46:14 -0700

    Closes SDK-CR-2

--- src/main/java/net/tridentsdk/util/Tuple.java
+++ src/main/java/net/tridentsdk/util/Tuple.java
@@ -16,29 +16,28 @@
  */
 package net.tridentsdk.util;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import net.tridentsdk.doc.Policy;
+
+import javax.annotation.concurrent.Immutable;
+
 /**
  * A class containing two separate data values
  * Usually used in return statements or as values in maps
  *
+ * <p>Tuples CANNOT be used as keys in Maps on account for
+ * the fact that all constructed Tuples are completely
+ * unique.</p>
+ *
  * @param <T> The first (A) instance type
  * @param <M> The second (B) instance type
  */
+@Immutable
+@Getter
+@RequiredArgsConstructor
+@Policy("no hashCode/equals, will break Cache functionality")
 public class Tuple<T, M> {
-    
     private final T a;
     private final M b;
-    
-    public Tuple(T a, M b){
-        this.a = a;
-        this.b = b;
-    }
-    
-    public T getA(){
-        return this.a;
-    }
-    
-    public M getB(){
-        return this.b;
-    }
-    
 }
