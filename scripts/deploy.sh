ddd6148f96bc893f89a7f97760a1691a2a235a08
Vilsol <vilsol2000@gmail.com>
2017-07-18 20:46:22 +0300

    Build and test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@ echo "Currently on branch: $TRAVIS_BRANCH"
 
 if [ "$TRAVIS_BRANCH" == "revamp" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ];
 then
-    ./gradlew jacocoTestReport uploadArchives
+    ./gradlew build jacocoTestReport uploadArchives
 
     # Publish coverage report
     bash <(curl -s https://codecov.io/bash)
