d571044cfb2a64153ccc9a6913cadaab42891397
Vilsol <vilsol2000@gmail.com>
2017-07-18 18:41:55 +0300

    Cleanup build and deployment

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,1 @@
-echo "Currently on branch: $TRAVIS_BRANCH"
-
-if [ "$TRAVIS_BRANCH" == "bleeding-edge" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ];
-then
-    echo "Compiling with deployment"
-    gradlew clean build jar jacocoTestReport uploadArchives
-else
-    echo "Compiling without deployment"
-    gradlew clean build jar jacocoTestReport
-fi
+./gradlew clean build jar
---
+++ scripts/deploy.sh
@@ -0,0 +1,9 @@
+echo "Currently on branch: $TRAVIS_BRANCH"
+
+if [ "$TRAVIS_BRANCH" == "revamp" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ];
+then
+    gradlew jacocoTestReport uploadArchives
+
+    # Publish coverage report
+    bash <(curl -s https://codecov.io/bash)
+fi
